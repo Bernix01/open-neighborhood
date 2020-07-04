18abe0c5f359c4c599c32d16bde0ae78c0ab11a3
Guillermo Bernal <gbernal096@gmail.com>
2020-07-04 11:49:03 -0500

    mockds for development

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
 ReactDOM.render(
   <React.StrictMode>
     <App />
