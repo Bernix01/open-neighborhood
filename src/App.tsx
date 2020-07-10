3aa38327cc4cb41ea3dbaddef0e2d5ec3cfd17ed
kbermude <kmbmha@hotmail.com>
2020-07-10 17:04:51 -0500

    login

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,12 @@ import logo from './logo.svg';
 import './App.css';
 import PostsComponent from './components/posts';
 import { client } from './GraphQLClient';
+import Login from './components/LoginForm';
 
 function App() {
   return (
     <ClientContext.Provider value={client}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <PostsComponent />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Login />
     </ClientContext.Provider>
   );
 }
