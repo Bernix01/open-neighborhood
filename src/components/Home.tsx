91199a566df83173d349833404a2bedd946699e1
Guillermo Bernal <gbernal096@gmail.com>
2020-07-15 22:22:06 -0500

    home (semi improved)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,45 +1,139 @@
-import React from 'react';
-import { Heading, Anchor, Box, Grommet, Header, Nav, Menu } from 'grommet';
+import React, { useRef, useState } from 'react';
 
-function Home() {
+import {
+  Avatar,
+  Button,
+  Box,
+  Drop,
+  grommet,
+  Grommet,
+  Nav,
+  Sidebar,
+  Grid,
+  Header,
+  Heading,
+} from 'grommet';
+
+import { Analytics, Calculator, Home, Stakeholder } from 'grommet-icons';
+
+const src = '//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80';
+
+// const NotificationIcon = () => (
+//   <Stack anchor="top-right">
+//     <Notification />
+//     <Box background="accent-1" pad="xsmall" round responsive={false} />
+//   </Stack>
+// );
+
+const SidebarFooter = () => (
+  <Box>
+    <Avatar margin="small" src={src} />
+  </Box>
+);
+
+const SidebarHeader = () => (
+  <Box pad="small">
+    <Avatar border={{ color: 'white', size: 'small' }} round="medium">
+      <Home color="white" />
+    </Avatar>
+  </Box>
+);
+
+const iconsMap = (color: string) => [
+  <Analytics key="0" color={color} />,
+  <Stakeholder key="1" color={color} />,
+  <Calculator key="2" color={color} />,
+];
+
+const SidebarButton = ({
+  iconName,
+  index,
+}: {
+  iconName: string;
+  index: number;
+}) => {
+  const [over, setOver] = useState<boolean>();
+  const tooltipColor = { color: 'accent-1', opacity: 0.9 };
+
+  const ref = useRef<HTMLButtonElement>(null);
   return (
-    <>
-      <Grommet>
-        <Header background="dark-1" pad="medium">
-          <Box direction="row" align="center" gap="small"></Box>
-          <Nav direction="row">
-            <Anchor href="#" label="Perfil" key="Perfil" />
-            <Menu
-              label="Residentes"
-              items={[
-                { label: 'Registrar', onClick: () => {} },
-                { label: 'Consultar ', onClick: () => {} },
-              ]}
-            />
-            <Menu
-              label="Personal de Seguridad"
-              items={[
-                { label: 'Registrar', onClick: () => {} },
-                { label: 'Consultar ', onClick: () => {} },
-              ]}
-            />
-            <Menu
-              label="Rubros"
-              items={[
-                { label: 'Crear', onClick: () => {} },
-                { label: 'Consultar ', onClick: () => {} },
-              ]}
-            />
-
-            <Anchor href="#" label="Reportes" key="Reportes" />
-          </Nav>
-        </Header>
-        <Heading alignSelf="center" textAlign="center">
-          Bienvenido a la Aplicación
-        </Heading>
-      </Grommet>
-    </>
+    <Box fill="horizontal">
+      <Button
+        ref={ref}
+        onMouseOver={() => setOver(true)}
+        onMouseLeave={() => setOver(false)}
+        onFocus={() => setOver(false)}
+        onBlur={() => setOver(false)}
+        hoverIndicator={tooltipColor}
+        plain
+      >
+        {({ hover }: { hover: boolean }) => {
+          return (
+            <Box pad={{ vertical: 'small' }} align="center">
+              {iconsMap(hover ? 'black' : 'white')[index]}
+            </Box>
+          );
+        }}
+      </Button>
+      {ref.current && over && (
+        <Drop align={{ left: 'right' }} target={ref.current} plain>
+          <Box
+            animation="slideRight"
+            margin="xsmall"
+            pad="small"
+            background={tooltipColor}
+            round={{ size: 'medium', corner: 'right' }}
+          >
+            {iconName}
+          </Box>
+        </Drop>
+      )}
+    </Box>
   );
-}
+};
 
-export default Home;
+export default () => (
+  <Grommet theme={grommet} full>
+    <Grid
+      columns={['auto', 'flex']}
+      rows={['xxsmall', 'flex']}
+      areas={[
+        { name: 'nav', start: [1, 0], end: [1, 0] },
+        { name: 'sidebar', start: [0, 0], end: [0, 1] },
+        { name: 'main', start: [1, 1], end: [1, 1] },
+      ]}
+      gap="xsmall"
+      pad="small"
+      fill="vertical"
+    >
+      <Sidebar
+        overflow="auto"
+        background="brand"
+        header={<SidebarHeader />}
+        footer={<SidebarFooter />}
+        pad="none"
+        gridArea="sidebar"
+        round="medium"
+      >
+        <Nav>
+          {['Analytics', 'Stakeholder', 'Calculator'].map((iconName, index) => (
+            <SidebarButton key={iconName} iconName={iconName} index={index} />
+          ))}
+        </Nav>
+      </Sidebar>
+      <Box align="center" justify="center" gridArea="main" />
+      <Header
+        align="center"
+        direction="row"
+        flex={false}
+        justify="between"
+        gap="medium"
+        gridArea="nav"
+      >
+        <Heading level="1" size="medium" textAlign="start" color="black">
+          Bienvenido a la aplicación
+        </Heading>
+      </Header>
+    </Grid>
+  </Grommet>
+);
