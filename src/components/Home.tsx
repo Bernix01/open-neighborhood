5e781f7356fe0b52b8cbf774addc8493bc75a3ac
Guillermo Enrique Bernal Moreira <gbernal096@gmail.com>
2020-07-29 18:49:18 -0500

    sprint 1 (#4)
    * login
    
    * login with mutation
    
    * updated login mutation to conform backend signature
    
    * use simplified fragment tag
    
    * home
    
    * home 1
    
    * home 2
    
    * login ui
    
    * change page titel
    
    * home (semi improved)
    
    * save changes
    
    Co-authored-by: kbermude <kmbmha@hotmail.com>

---
+++ src/components/Home.tsx
@@ -0,0 +1,8 @@
+import React, { useRef, useState } from 'react';
+import DashboardLayout from './layouts/main';
+
+export default () => (
+  <DashboardLayout>
+    <p>Home</p>
+  </DashboardLayout>
+);
