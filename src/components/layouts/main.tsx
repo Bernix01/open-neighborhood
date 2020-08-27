7a154807ada7fd43b2ecca92a96f5bb85460dcd7
kbermude <kmbmha@hotmail.com>
2020-08-27 11:57:45 -0500

    Revert "cambios de PR"
    This reverts commit 4d7c6a45a01bb72b6fa73d39f366eabbb455a1e7.

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, MouseEventHandler } from 'react';
 
 import {
   Avatar,
@@ -14,7 +14,16 @@ import {
   Heading,
 } from 'grommet';
 
-import { Analytics, Calculator, Home, Stakeholder } from 'grommet-icons';
+import {
+  Analytics,
+  Calculator,
+  Home,
+  Stakeholder,
+  Edit,
+  Trash,
+  Add,
+} from 'grommet-icons';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 const src = '//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80';
 
@@ -57,55 +66,80 @@ const SidebarButtonIcon: React.FC<{ hover: boolean; index: number }> = ({
 };
 
 const SidebarButton: React.FC<{
-  iconName: string;
+  title: string;
+  to: string;
   index: number;
-}> = ({ iconName, index }) => {
+}> = ({ title, index, to }) => {
+  const match = useRouteMatch({
+    path: to,
+    strict: true,
+    exact: to === '/',
+    sensitive: true,
+  });
   const [over, setOver] = useState<boolean>(false);
   const tooltipColor = { color: 'accent-1', opacity: 0.9 };
 
   const ref = useRef<HTMLButtonElement>(null);
   return (
-    <Box fill="horizontal">
-      <Button
-        ref={ref}
-        onMouseOver={() => setOver(true)}
-        onMouseLeave={() => setOver(false)}
-        onFocus={() => setOver(false)}
-        onBlur={() => setOver(false)}
-        hoverIndicator={tooltipColor}
-        plain
-      >
-        <SidebarButtonIcon hover={over} index={index} />
-      </Button>
-      {ref.current && over && (
-        <Drop align={{ left: 'right' }} target={ref.current} plain>
-          <Box
-            animation="slideRight"
-            margin="xsmall"
-            pad="small"
-            background={tooltipColor}
-            round={{ size: 'medium', corner: 'right' }}
-          >
-            {iconName}
-          </Box>
-        </Drop>
-      )}
-    </Box>
+    <Link to={to}>
+      <Box fill="horizontal">
+        <Button
+          ref={ref}
+          onMouseOver={() => setOver(true)}
+          onMouseLeave={() => setOver(false)}
+          onFocus={() => setOver(false)}
+          onBlur={() => setOver(false)}
+          hoverIndicator={tooltipColor}
+          active={match !== null}
+          plain
+        >
+          <SidebarButtonIcon hover={over} index={index} />
+        </Button>
+        {ref.current && over && (
+          <Drop align={{ left: 'right' }} target={ref.current} plain>
+            <Box
+              animation="slideRight"
+              margin="xsmall"
+              pad="small"
+              background={tooltipColor}
+              round={{ size: 'medium', corner: 'right' }}
+            >
+              {title}
+            </Box>
+          </Drop>
+        )}
+      </Box>
+    </Link>
   );
 };
 
-const DashboardLayout: React.FunctionComponent = ({ children }) => (
+const routes = [
+  { icon: 'Analytics', title: 'Dashboard', to: '/' },
+  { icon: 'Stakeholder', title: 'Residentes', to: '/residents' },
+  { icon: 'Calculator', title: 'Alícuotas', to: '/' },
+];
+
+interface Action {
+  type: 'add' | 'delete' | 'edit';
+  onClick?: MouseEventHandler;
+  to?: string;
+}
+
+const DashboardLayout: React.FunctionComponent<{ actions?: Action[] }> = ({
+  children,
+  actions,
+}) => (
   <Grommet theme={grommet} full>
     <Grid
       columns={['auto', 'flex']}
-      rows={['xxsmall', 'flex']}
+      rows={['auto', 'flex']}
       areas={[
         { name: 'nav', start: [1, 0], end: [1, 0] },
         { name: 'sidebar', start: [0, 0], end: [0, 1] },
         { name: 'main', start: [1, 1], end: [1, 1] },
       ]}
-      gap="xsmall"
-      pad="small"
+      gap="small"
+      pad="xsmall"
       fill="vertical"
     >
       <Sidebar
@@ -118,14 +152,17 @@ const DashboardLayout: React.FunctionComponent = ({ children }) => (
         round="medium"
       >
         <Nav>
-          {['Analytics', 'Stakeholder', 'Calculator'].map((iconName, index) => (
-            <SidebarButton key={iconName} iconName={iconName} index={index} />
+          {routes.map((route, index) => (
+            <SidebarButton
+              to={route.to}
+              key={route.title}
+              title={route.title}
+              index={index}
+            />
           ))}
         </Nav>
       </Sidebar>
-      <Box align="center" justify="center" gridArea="main">
-        {children}
-      </Box>
+      <Box gridArea="main">{children}</Box>
       <Header
         align="center"
         direction="row"
@@ -134,12 +171,42 @@ const DashboardLayout: React.FunctionComponent = ({ children }) => (
         gap="medium"
         gridArea="nav"
       >
-        <Heading level="1" size="medium" textAlign="start" color="black">
-          Bienvenido a la aplicación
+        <Heading level="2" size="medium" textAlign="start" color="black">
+          Urbanización La alameda
         </Heading>
+        <Box direction="row-responsive" justify="end" align="end">
+          {actions &&
+            actions.map((action) => {
+              if (action.to) {
+                return <Link to={action.to}>{getActionComponent(action)}</Link>;
+              }
+              return getActionComponent(action);
+            })}
+        </Box>
       </Header>
     </Grid>
   </Grommet>
 );
+
+function getActionComponent(action: Action) {
+  switch (action.type) {
+    case 'edit':
+      return <Button key={action.type} label="editar" icon={<Edit />} plain />;
+    case 'delete':
+      return <Button label="borrar" key={action.type} icon={<Trash />} plain />;
+    case 'add':
+      return (
+        <Button
+          label="agregar"
+          onClick={action.onClick}
+          key={action.type}
+          icon={<Add />}
+          plain
+        />
+      );
+    default:
+      return <span>unsuported action</span>;
+  }
+}
 
 export default DashboardLayout;
--- src/components/registroStaff.tsx
+++ src/components/registroStaff.tsx
@@ -14,7 +14,22 @@ import {
 import { Lock, LinkNext } from 'grommet-icons';
 import { useMutation } from 'graphql-hooks';
 import { useForm } from 'react-hook-form';
-import DashboardLayout from './layouts/main';
+import { login } from 'src/Auth';
+
+const theme: ThemeType = {
+  global: {
+    colors: {
+      background: {
+        light: '#ffffff',
+        dark: '#000000',
+      },
+    },
+    font: {
+      family:
+        '-apple-system,\n         BlinkMacSystemFont, \n         "Segoe UI", \n         Roboto, \n         Oxygen, \n         Ubuntu, \n         Cantarell, \n         "Fira Sans", \n         "Droid Sans",  \n         "Helvetica Neue", \n         Arial, sans-serif,  \n         "Apple Color Emoji", \n         "Segoe UI Emoji", \n         "Segoe UI Symbol"',
+    },
+  },
+};
 
 type FormData = {
   user: string;
@@ -38,61 +53,74 @@ export default () => {
   const { register } = useForm<FormData>();
 
   return (
-    <DashboardLayout>
-      <Heading level="3">Registro Residente</Heading>
-      <Form>
-        <FormField name="id_card" label="Cédula" required>
-          <TextInput name="id_card" ref={register} />
-        </FormField>
-        <FormField name="name" label="Nombre" required>
-          <TextInput name="name" ref={register} />
-        </FormField>
-        <FormField name="birth" label="Fecha de nacimiento" required>
-          <DateInput format="mm/dd/yyyy" value={new Date().toISOString()} />
-        </FormField>
-        <FormField
-          name="telefono"
-          label="Telefono"
-          placeholder="Telefono"
-          required
-        >
-          <TextInput name="telefono" pattern="[0-9]{10}" ref={register} />
-        </FormField>
-        <FormField
-          name="direccion"
-          label="Address"
-          placeholder="Direccion"
-          required
-        >
-          <TextInput name="address" ref={register} />
-        </FormField>
-        <FormField name="user" label="Correo" required>
-          <TextInput name="user" type="email" ref={register} />
-        </FormField>
-        <FormField
-          name="password"
-          maxLength={50}
-          minLength={4}
-          label="Contraseña"
-          required
-        >
-          <TextInput
+    <Grommet full theme={theme}>
+      <Main
+        fill="vertical"
+        flex="grow"
+        overflow="auto"
+        justify="center"
+        direction="column"
+        align="center"
+      >
+        <Heading level="3" textAlign="center">
+          Registro Staff
+        </Heading>
+        <Form>
+          <FormField
+            name="birth"
+            label="Birth"
+            placeholder="Fecha de Nacimiento"
+            required
+          >
+            <DateInput format="mm/dd/yyyy" value={new Date().toISOString()} />
+          </FormField>
+          <FormField
+            name="telefono"
+            label="Telefono"
+            placeholder="Telefono"
+            required
+          >
+            <TextInput name="telefono" pattern="[0-9]{10}" ref={register} />
+          </FormField>
+          <FormField
+            name="id_card"
+            label="Address"
+            placeholder="Direccion"
+            required
+          >
+            <TextInput name="address" ref={register} />
+          </FormField>
+          <FormField name="name" label="Name" placeholder="Nombre" required>
+            <TextInput name="name" ref={register} />
+          </FormField>
+          <FormField name="user" label="Correo" placeholder="Correo" required>
+            <TextInput name="user" type="email" ref={register} />
+          </FormField>
+          <FormField
             name="password"
-            icon={<Lock />}
-            ref={register}
-            placeholder="****"
-            type="password"
-          />
-        </FormField>
+            maxLength={50}
+            minLength={4}
+            label="Contraseña"
+            required
+          >
+            <TextInput
+              name="password"
+              icon={<Lock />}
+              ref={register}
+              placeholder="****"
+              type="password"
+            />
+          </FormField>
 
-        <Button
-          label="Registrar"
-          icon={<LinkNext />}
-          primary
-          secondary={false}
-          type="submit"
-        />
-      </Form>
-    </DashboardLayout>
+          <Button
+            label="Registrar"
+            icon={<LinkNext />}
+            primary
+            secondary={false}
+            type="submit"
+          />
+        </Form>
+      </Main>
+    </Grommet>
   );
 };
