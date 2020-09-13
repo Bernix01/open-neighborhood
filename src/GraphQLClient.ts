2422dd9eeca9e1a6d0845eb83654ec096c544073
Guillermo Bernal <gbernal096@gmail.com>
2020-09-13 11:48:04 -0500

    include csrf token

--- src/GraphQLClient.ts
+++ src/GraphQLClient.ts
@@ -1,7 +1,12 @@
 import { GraphQLClient } from 'graphql-hooks';
 import { authFetch } from './Auth';
 
+const csrftoken: HTMLInputElement | null = document.querySelector(
+  '[name=csrfmiddlewaretoken]'
+);
+
 export const client = new GraphQLClient({
   url: 'http://localhost:8000/graphql/',
   fetch: (...args) => authFetch(...args),
+  headers: { 'X-CSRFToken': csrftoken?.value ?? '' },
 });
